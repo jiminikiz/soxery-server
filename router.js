0c8b7f6ff25c33d3d2b71e723d6f7ab5df75a31d
Bjimz <brandon.jimenez@iterate.ai>
2020-12-21 11:00:29 -0700

    Clean

--- router.js
+++ router.js
@@ -2,12 +2,10 @@ const logger = require('koa-logger');
 const Router = require('koa-router');
 const Static = require('koa-static');
 const routes = require('./routes');
-
 const router = new Router();
+const mountRoute = ({ method, path, handler }) => router[method](path, handler);
 
-routes.forEach(
-  (route) => router[route.method](route.path, route.handler)
-);
+routes.forEach(mountRoute);
 
 module.exports = (app) => app
   .use(logger())
